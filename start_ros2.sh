c0b8224232f059dbaab1aa4b08570235fac0d839
Hwanhong Lee <hwanhong57@gmail.com>
2025-01-15 13:58:12 +0900

    op_carla/op_agent -b ros2, edit by h2_i

--- start_ros2.sh
+++ start_ros2.sh
@@ -12,8 +12,10 @@ echo $explore_mode
 echo $route_topic
 echo "-------------------------------"
 
-source /home/hatem/hatem-repos/autoware-universe/autoware/install/setup.bash
+#source /home/hatem/hatem-repos/autoware-universe/autoware/install/setup.bash
+# HH_230906
+source /home/a/carla-autoware-universe/autoware/install/setup.bash
 
 #ros2 launch ${TEAM_CODE_ROOT}/autoware_launch/universe.launch.xml map_path:=/home/hatem/Data/autoware_uni_sample_data/${map_name} vehicle_model:=sample_vehicle sensor_model:=sample_sensor_kit
-ros2 launch /home/hatem/hatem-repos/autoware-universe/autoware/src/launcher/autoware_launch/autoware_launch/launch/autoware.launch.xml map_path:=/home/hatem/Data/autoware_uni_sample_data/${map_name} vehicle_model:=sample_vehicle sensor_model:=sample_sensor_kit
+ros2 launch /home/a/carla-autoware-universe/autoware/src/launcher/autoware_launch/autoware_launch/launch/autoware.launch.xml map_path:=/home/a/Downloads/sample-planning-map vehicle_model:=sample_vehicle sensor_model:=sample_sensor_kit
 #$SHELL
